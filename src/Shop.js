782ac9fccf683916204ab1997cbdbd0f1f84a551
aliya <aliyalewis5@gmail.com>
2019-12-07 17:47:53 -0800

    attempted to use useState and useEffect to fetch json from an api. don't think the api url is working.

--- src/Shop.js
+++ src/Shop.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function Shop() {
+
+  useEffect(() => {
+    fetchItems();
+  })
+
+  const fetchItems = async() => {
+    const data = await fetch("https://fortnite-public-api.theapinetwork.com/prod09/upcoming/get")
+    const items = await data.json()
+    
+    console.log(items);
+  }
+
   return (
     <div className="Shop">
       <h1>Shop Page</h1>
