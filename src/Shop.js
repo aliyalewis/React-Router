2ae5591924ef5d6b0303b6af9d584b4923160435
aliya <aliyalewis5@gmail.com>
2019-12-07 18:07:47 -0800

    havin an issue with the map function for the result of the fetch in Shop.js, probably not returning a json object.

--- src/Shop.js
+++ src/Shop.js
@@ -7,16 +7,22 @@ function Shop() {
     fetchItems();
   })
 
+  const [items, setItems] = useState([]);
+
   const fetchItems = async () => {
     const data = await fetch("https://dog.ceo/api/breeds/image/random");
     const items = await data.json();
     
     console.log(items);
+    setItems(items.message);
   }
+
 
   return (
     <div className="Shop">
-      <h1>Shop Page</h1>
+      {items.map(item => (
+        <img src={item}/>
+      ))}
     </div>
   );
 }
