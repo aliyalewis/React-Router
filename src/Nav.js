0b4653fb977cdc6b5661fdb8a31d7144126ad5bb
aliya <aliyalewis5@gmail.com>
2019-12-07 17:30:14 -0800

    added clickable links in the nav bar with Link from react-router-dom

--- src/Nav.js
+++ src/Nav.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './App.css';
+import { Link } from'react-router-dom';
 
 function Nav() {
+
+  const navStyle = {
+    color: "white"
+  }
+
   return (
     <div className="nav">
       <nav>
         <h3>Logo</h3>
         <ul className = "nav-links">
-          <li>About</li>
-          <li>Shop</li>
+          <Link style={navStyle} to="/about">
+            <li>About</li>
+          </Link>
+          <Link style={navStyle} to="/shop">
+            <li>Shop</li>
+          </Link>
         </ul>
       </nav>
     </div>
